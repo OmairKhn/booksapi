0e268ffa125e87acf8424b70a6e29f6df523aa27
w3schools-test <test@w3schools.com>
2023-09-17 13:07:55 -0700

    test controller

---
+++ Models/testcontroller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreApi.Models
+{
+    public class testcontroller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
